9b783b641ed0b74afb78fbad46046cdbcae79836
dieterch <dieter.chvatal@gmail.com>
2024-03-07 16:29:46 +0100

    names added

--- app.py
+++ app.py
@@ -7,6 +7,39 @@ mac = "10:bf:48:8d:5c:34"
 app = Quart(__name__)
 
 keys = ['timestamp', 'mac', 'ip', 'hostname', 'id']
+zdata = {
+    'shellyplug-s-040D25' : {
+        'name' : 'Blumenlicht',
+        'gen':1 
+    },
+    'shellyplug-s-6A6374' : {
+        'name' : 'BeoCreate',
+        'gen':1 
+    },
+    'shelly1pmminig3-ecda3bc405dc' : {
+        'name' : 'Wohnzimmer Deckenlicht',
+        'gen':3 
+    },
+    'shelly1-5B2B93' : {
+        'name' : 'Gang Deckenlicht',
+        'gen':1 
+    },
+    'shellyplus1-08b61fd93e1c' : {
+        'name' : 'Küche Schalter oben',
+        'gen':2 
+    },
+    'shellyplus1-08b61fd7f0ac' : {
+        'name' : 'Schlafzimmer Dieter Türschalter',
+        'gen':2 
+    },
+    'shellyplus2pm-5443b23e53b8' : {
+        'name' : 'Küche & Esstisch',
+        'gen':2 
+    },
+  
+}
+
+
 
 @app.route('/wakeonlan')
 async def wakeonlan():
@@ -27,6 +60,8 @@ async def index():
         lines = text_file.readlines()
         for line in lines:
             rec = dict(zip(keys, line.strip().split()))
+            if rec['hostname'] in zdata:
+                rec['name'] = zdata[rec['hostname']]['name']
             data.append(rec)
             print(line.strip())
         # print(len(lines))
@@ -36,4 +71,4 @@ async def index():
         print("File not found")
     return await render_template("index.html", data=data)  # Required to be in templates/
 
-app.run(host='0.0.0.0', port=5550, debug=True)
+app.run(host='0.0.0.0', port=5500, debug=True)
